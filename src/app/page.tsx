7a9e82aed145bd64a9c8f65ba2a901e6047d66f9
amandaht99 <amanda.hamblin-true@code.berlin>
2023-10-26 00:39:06 +0200

    feat: landing page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,115 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import {
+  Box,
+  Heading,
+  Text,
+  VStack,
+  SimpleGrid,
+  Container,
+  Icon,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+import { FaEuroSign, FaHome, FaGlobeAmericas, FaUsers } from "react-icons/fa";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
-export default function Home() {
+// Framer motion variants for the animation
+const variants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+const MotionBox = motion(Box);
+
+// Define color scheme
+const colors = {
+  brand: {
+    900: "#F13B07",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+});
+
+export default function Page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <ChakraProvider theme={theme}>
+      <Box bg="white" color="black">
+        <Container maxW="container.xl" py={10}>
+          <VStack spacing={8} align="start">
+            <Heading as="h1" size="2xl" color="brand.900">
+              Welcome to Roomy!
+            </Heading>
+            <Text fontSize="lg">
+              Immerse yourself in a new culture, live like a local, and save on
+              accommodation costs - all with Roomy! At Roomy, we believe travel
+              is not just about seeing new places, but experiencing them like a
+              local. We are your gateway to authentic travel experiences,
+              providing a platform to swap homes with others who share your
+              wanderlust.
+            </Text>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Heading as="h2" size="xl">
+              Why Choose Roomy?
+            </Heading>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10} mt={5}>
+              {[
+                {
+                  title: "Cost-Effective Travel",
+                  description:
+                    "Swap homes instead of spending on hotels. Say goodbye to expensive rentals, and embrace a cost-effective way of exploring the world.",
+                  icon: FaEuroSign,
+                },
+                {
+                  title: "Authentic Experiences",
+                  description:
+                    "Live where the locals live. Experience cities, neighbourhoods, and lifestyles - not as tourists, but as residents.",
+                  icon: FaHome,
+                },
+                {
+                  title: "Trusted Community",
+                  description:
+                    "Our members understand and value the trust needed to open their homes. We ensure a safe and reliable home-swapping experience.",
+                  icon: FaUsers,
+                },
+                {
+                  title: "Global Access",
+                  description:
+                    "With properties across the globe, your dream vacation is just a swap away. City apartments, country cottages, beachfront villas? You name it!",
+                  icon: FaGlobeAmericas,
+                },
+              ].map((card, index) => (
+                <MotionBox
+                  key={index}
+                  p={5}
+                  bg="white"
+                  boxShadow="md"
+                  rounded="md"
+                  initial="hidden"
+                  animate="show"
+                  variants={variants}
+                  whileHover={{ y: -10 }}
+                >
+                  <Flex direction="column" align="start" spacing={3}>
+                    <Icon as={card.icon} boxSize={6} color="brand.900" />
+                    <Heading as="h3" size="md" color="brand.900">
+                      {card.title}
+                    </Heading>
+                    <Text>{card.description}</Text>
+                  </Flex>
+                </MotionBox>
+              ))}
+            </SimpleGrid>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <Button colorScheme="red" size="lg">
+              Join Roomy today - Travel more, spend less!
+            </Button>
+          </VStack>
+        </Container>
+      </Box>
+    </ChakraProvider>
+  );
 }
