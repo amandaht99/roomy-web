69fbd24539ef77041951af6fdbf58a3a383bf360
amandaht99 <amanda.hamblin-true@code.berlin>
2023-10-29 16:27:10 +0100

    feat: profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+"use client";
+// Profile.js
+import { Box, Heading, VStack, SimpleGrid, Container } from "@chakra-ui/react";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import PropertyInfo from "../../components/property-info";
+import UserInfo from "../../components/user-info";
 
-function Profile() {
-  return <div>Profile</div>;
+const property = {
+  image: [
+    "https://via.placeholder.com/300",
+    "https://via.placeholder.com/300",
+    "https://via.placeholder.com/300",
+  ],
+  name: "Beautiful Apartment",
+  description:
+    "Some description about the property. This could include details about the location, amenities, size etc.",
+  location: "Property Location, City",
+  reviews: [
+    "Great location, near to several tourist attractions.",
+    "The apartment was clean and spacious.",
+    "I loved the amenities provided.",
+  ],
+};
+
+const user = {
+  name: "Jane Doe",
+  description:
+    "Some brief description about the user. This might include their interests, languages spoken etc.",
+  email: "jane.doe@example.com",
+  phone: "+123456789",
+  profilePicture: "",
+};
+
+// Define color scheme
+const colors = {
+  brand: {
+    900: "#F13B07",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+});
+
+export default function Profile() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box bg="white" color="black">
+        <Container maxW="container.xl" py={10}>
+          <VStack spacing={8} align="start">
+            <Heading as="h1" size="2xl" color="brand.900">
+              Property Profile
+            </Heading>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10} mt={5}>
+              <PropertyInfo property={property} />
+              <UserInfo user={user} />
+            </SimpleGrid>
+          </VStack>
+        </Container>
+      </Box>
+    </ChakraProvider>
+  );
 }
-
-export default Profile;
---
+++ src/components/property-info.tsx
@@ -0,0 +1,84 @@
+"use client";
+import {
+  Box,
+  Heading,
+  Text,
+  VStack,
+  Image,
+  Icon,
+  Skeleton,
+  List,
+  ListItem,
+  ListIcon,
+  Button,
+} from "@chakra-ui/react";
+import { FaMapMarkerAlt, FaStar, FaPen } from "react-icons/fa";
+import { motion } from "framer-motion";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+const MotionBox = motion(Box);
+
+// Framer motion variants for the animation
+const variants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+const PropertyInfo = ({ property }) => {
+  return (
+    <MotionBox
+      p={5}
+      bg="white"
+      boxShadow="md"
+      rounded="md"
+      initial="hidden"
+      animate="show"
+      variants={variants}
+      whileHover={{ y: -10 }}
+    >
+      <VStack align="start" spacing={5}>
+        <Skeleton
+          isLoaded={property && property.images && property.images.length}
+        >
+          <Carousel showThumbs={false}>
+            {property &&
+              property.images &&
+              property.images.map((img, index) => (
+                <div key={index}>
+                  <Image src={img} alt={`Property ${index + 1}`} />
+                </div>
+              ))}
+          </Carousel>
+        </Skeleton>
+        <Heading as="h2" size="lg" color="#F13B07">
+          {property.name}
+        </Heading>
+        <Text>{property.description}</Text>
+        <Text>
+          <Icon as={FaMapMarkerAlt} color="#F13B07" /> {property.location}
+        </Text>
+        <Heading as="h3" size="md" color="#F13B07">
+          Reviews
+        </Heading>
+        <List spacing={2}>
+          {property.reviews.map((review, index) => (
+            <ListItem key={index}>
+              <ListIcon as={FaStar} color="yellow.400" />
+              {review}
+            </ListItem>
+          ))}
+        </List>
+        <Button
+          leftIcon={<Icon as={FaPen} />}
+          colorScheme="red"
+          variant="outline"
+        >
+          Edit Property Info
+        </Button>
+      </VStack>
+    </MotionBox>
+  );
+};
+
+export default PropertyInfo;
